573f9462cceff332e6fde6364d9ffd4e4aecc539
Iron-Foot <riley@sligting.com>
2024-10-08 23:51:26 -0600

    I did loads of stuff.

---
+++ js/circular Mouse Pointer.js
@@ -0,0 +1,37 @@
+const circlePointer = document.getElementById('circlePointer');
+let isWhiteStyle = false;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const linkTags = document.getElementsByTagName('link');
+    for (let link of linkTags) {
+        if (link.href && link.href.includes('stylesWhite.css')) {
+            isWhiteStyle = true;
+            circlePointer.style.backgroundColor = '#000';
+        }
+    }
+});
+
+document.addEventListener('mousemove', function(e) {
+    window.requestAnimationFrame(() => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        circlePointer.style.left = `${mouseX}px`;
+        circlePointer.style.top = `${mouseY}px`;
+    });
+});
+
+document.addEventListener('mouseover', function(e) {
+    if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON' || e.target.hasAttribute('role') && e.target.getAttribute('role') === 'button' || e.target.type === 'button' || e.target.type === 'submit') {
+        circlePointer.style.width = '40px';
+        circlePointer.style.height = '40px';
+        circlePointer.style.backgroundColor = isWhiteStyle ? '#000' : '#fff';
+    }
+});
+
+document.addEventListener('mouseout', function(e) {
+    if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON' || e.target.hasAttribute('role') && e.target.getAttribute('role') === 'button' || e.target.type === 'button' || e.target.type === 'submit') {
+        circlePointer.style.width = '20px';
+        circlePointer.style.height = '20px';
+        circlePointer.style.backgroundColor = isWhiteStyle ? '#000' : '#fff';
+    }
+});
